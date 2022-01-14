3b1ffbd5cdc9beb394d3dfbc5171e71f962f01d7
Francesco Pedron <francesco.pedron@omnys.com>
2022-01-14 10:05:06 +0100

    Build working

--- src/nativeMain/kotlin/Main.kt
+++ src/nativeMain/kotlin/Main.kt
@@ -2,16 +2,18 @@ import libcurl.*
 import kotlinx.cinterop.*
 
 fun main() {
-    println("Hello 3, Kotlin/Native!")
+    println("Hello 4, Kotlin/Native!")
     val curl = curl_easy_init()  ?: throw RuntimeException("Could not initialize an easy handle")
-    /*val curl = curl_easy_init()
-    if(curl != null) {
+
+
         curl_easy_setopt(curl, CURLOPT_URL, "http://jonnyzzz.com")
         curl_easy_setopt(curl, CURLOPT_FOLLOWLOCATION, 1L)
         val res = curl_easy_perform(curl)
         if (res != CURLE_OK) {
             println("curl_easy_perform() failed ${curl_easy_strerror(res)?.toKString()}")
+        } else {
+            println("curl_easy_perform() ok!")
         }
         curl_easy_cleanup(curl)
-    }*/
+
 }
